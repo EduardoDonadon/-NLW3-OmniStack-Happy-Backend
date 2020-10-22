aad9c2f464852c08c38798146023b313ca0954b8
EduardoDonadon <dona.edu2@gmail.com>
2020-10-22 00:00:25 -0300

    deploying api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import path from 'path';
 import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
-import path from 'path';
 import cors from 'cors';
 
 import './database/connection';
