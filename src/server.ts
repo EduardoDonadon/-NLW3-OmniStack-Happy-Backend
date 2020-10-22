56eee6bf4c6965d73d42b4370ea62072c2084f7a
EduardoDonadon <dona.edu2@gmail.com>
2020-10-21 23:30:54 -0300

    deploying api

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
@@ -13,6 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
